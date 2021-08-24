2fb8446e3b8b052e70bf02fba58ac9a01ec8ac31
soft pvt <soft pvt@DESKTOP-8VO1IUL>
2021-08-24 18:26:34 +0500

    Add project files.

---
+++ ChatAppUsingSignalr/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using ChatAppUsingSignalr.Data;
+using ChatAppUsingSignalr.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ChatAppUsingSignalr.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        public readonly ApplicationDbContext _context;
+        public readonly UserManager<AppUser> _userManager;
+
+    
+
+        public HomeController(UserManager<AppUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.CurrentUserName = currentUser.UserName;
+            }
+            var messages = await _context.Messages.ToListAsync();
+            return View(messages);
+        }
+
+        public async Task<IActionResult> Create(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                message.UserName = User.Identity.Name;
+                var sender = await _userManager.GetUserAsync(User);
+                message.UserID = sender.Id;
+                await _context.Messages.AddAsync(message);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return Error();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
